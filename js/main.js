e495e4a0aca9a57e8eb654104d48800a10a78c65
Aleksandra Szwedo <aleksandraa.szwedo@gmail.com>
2018-09-04 22:29:52 +0200

    working project

---
+++ js/main.js
@@ -0,0 +1,42 @@
+document.getElementById("countdown").addEventListener("click", function () {
+    var year = document.getElementById("year").value;
+    var month = document.getElementById("month").value - 1;
+    var day = document.getElementById("day").value;
+    var hour = document.getElementById("hour").value;
+    var minute = document.getElementById("minute").value;
+    var countDownDate = new Date(year, month , day, hour, minute).getTime();
+
+    var x = setInterval(function () {
+
+        var now = new Date().getTime();
+
+        var distance = countDownDate - now;
+
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        document.getElementById("count").innerHTML = days + " days " + hours + " : " +
+            minutes + " : " + seconds + " left for your event!";
+    });
+    
+    document.getElementById("form").classList.add("clear");
+    document.getElementById("count").classList.add("count-add");
+    
+})
+
+document.getElementById("holiday").addEventListener("click", function () {
+    document.getElementById("main").removeAttribute("class");
+    document.getElementById("main").classList.add("holiday");
+})
+
+document.getElementById("birthday").addEventListener("click", function () {
+    document.getElementById("main").removeAttribute("class");
+    document.getElementById("main").classList.add("birthday");
+})
+
+document.getElementById("wedding").addEventListener("click", function () {
+    document.getElementById("main").removeAttribute("class");
+    document.getElementById("main").classList.add("wedding");
+})
